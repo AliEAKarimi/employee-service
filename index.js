603ba42c5339aaefb1d141ff69e2cf5e32a99f5d
ali.karimi <ali.karimi@part.local>
2023-02-21 14:11:23 +0330

    Add basic server setup using HTTP module

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+    if(req.method === "POST" && req.url === )
+})
+
+server.listen(81, '127.0.0.1', () => {
+    console.log('Server listening on port 81')
+})
