894e5ddbc67b9ab46877f16aff7dd87716968419
ali.karimi <ali.karimi@part.local>
2023-02-25 13:56:25 +0330

    format document

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
-const http = require('http');
-const url = require('url');
-const redis = require('redis');
+const http = require("http");
+const url = require("url");
+const redis = require("redis");
 const client = redis.createClient();
 
 client.connect();
 client.on("connect", function () {
-    console.log("اتصال موفق به پایگاه داده");
+  console.log("اتصال موفق به پایگاه داده");
 });
 client.on("error", function (err) {
-    console.log(`خطا در ارتباط با پایگاه داده ${err}`);
+  console.log(`خطا در ارتباط با پایگاه داده ${err}`);
 });
 
 const server = http.createServer(async (req, res) => {
-    const { query, pathname } = url.parse(req.url, true);
-    const method = req.method;
-    if (pathname === "/dataService") {
-        if (method === "POST") {
-          let body = "";
-          req.on("data", (chunk) => {
-            body += chunk;
-          });
-          req.on("end", async () => {
-            const { id, data, parent } = JSON.parse(body);
-            try {
-              // if (!id || !data || !parent) throw new Error("درخواست نامعتبر است.");
-    
-              const isIdExists = await client.exists(`id:${id}:data`);
-              if (isIdExists) throw new Error("شناسه داده ها تکراری است.");
-    
-              const isParentExist = await client.exists(`id:${parent}:data`);
-              if (!isParentExist) throw new Error("شناسه والد نامعتبر است.");
-    
-              // Save data and parent to Redis
-              await client.set(`id:${id}:data`, data);
-              await client.set(`id:${id}:parent`, parent);
-    
-              res.end("داده ها ذخیره شد.");
-            } catch (error) {
-              res.statusCode = error.statusCode ?? 500;
-              res.end(error.message ?? "خطا در ارتباط با پایگاه داده");
-            }
-          });
+  const { query, pathname } = url.parse(req.url, true);
+  const method = req.method;
+  if (pathname === "/dataService") {
+    if (method === "POST") {
+      let body = "";
+      req.on("data", (chunk) => {
+        body += chunk;
+      });
+      req.on("end", async () => {
+        const { id, data, parent } = JSON.parse(body);
+        try {
+          // if (!id || !data || !parent) throw new Error("درخواست نامعتبر است.");
+
+          const isIdExists = await client.exists(`id:${id}:data`);
+          if (isIdExists) throw new Error("شناسه داده ها تکراری است.");
+
+          const isParentExist = await client.exists(`id:${parent}:data`);
+          if (!isParentExist) throw new Error("شناسه والد نامعتبر است.");
+
+          // Save data and parent to Redis
+          await client.set(`id:${id}:data`, data);
+          await client.set(`id:${id}:parent`, parent);
+
+          res.end("داده ها ذخیره شد.");
+        } catch (error) {
+          res.statusCode = error.statusCode ?? 500;
+          res.end(error.message ?? "خطا در ارتباط با پایگاه داده");
         }
+      });
     }
+  }
 });
 
 const serverPort = 81;
