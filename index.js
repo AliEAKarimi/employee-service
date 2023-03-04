be5921eaaa9fe22a3e34eca65be568f128a2b68a
ali.karimi <ali.karimi@part.local>
2023-03-04 09:29:42 +0330

    start the server

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+const Server = require("./server/server");
 const controller = require("./controllers/controller");
 const { RequestMethod } = require("./models/requestMethod");
 const Router = require("./router/router");
-
+const { isOperationalError } = require("./errorHandlers/errorHandler");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const router = new Router();
 // add routes
 router.addRoute("/dataService", RequestMethod.POST, controller.addUser);
 router.addRoute("/dataService", RequestMethod.GET, controller.getUser);
 router.addRoute("/dataService", RequestMethod.PUT, controller.updateUser);
+
+const server = new Server(
+  router.route.bind(router),
+  process.env.SERVER_HOST,
+  process.env.SERVER_PORT
+);
+// start server
+server.start();
