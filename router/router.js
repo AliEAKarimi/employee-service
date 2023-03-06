20a9e1af77eb0a68caaa28d182c29ffb48174dba
ali.karimi <ali.karimi@part.local>
2023-03-06 17:15:02 +0330

    remove unnecessary comments

--- router/router.js
+++ router/router.js
@@ -15,7 +15,6 @@ module.exports = class Router {
     console.log("Routing a request for  " + pathname);
     if (!this.handle[pathname]) {
       throw new NotFoundError(`No request handler found for ${pathname}`);
-      // throw new NotFoundError(`The path ${pathname} was not found`);
     } else {
       if (typeof this.handle[pathname][request.method] === "function") {
         this.handle[pathname][request.method](request, response);
