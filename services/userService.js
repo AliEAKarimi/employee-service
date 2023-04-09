fa910ca0cfa4b0ca97e490cdca577bc4409b39d5
Ali Karimi <alieakarimi@gmail.com>
2023-04-09 11:12:35 +0330

    implemented userService that contains some business logic codes

---
+++ services/userService.js
@@ -0,0 +1,32 @@
+const { userDB, parentDB } = require("../database/databases");
+const UserModel = require("../models/userModel");
+const DatabaseError = require("../errorHandlers/databaseError");
+module.exports = class UserService {
+  async addUser({ id, data, parent }) {
+    // Create user model
+    const user = new UserModel(id, data, parent);
+    try {
+      await user.save(userDB, parentDB);
+    } catch (error) {
+      throw new DatabaseError("Error in adding data");
+    }
+  }
+  async updateUser({ id, data, parent }) {
+    // Create user model
+    const user = new UserModel(id, data, parent);
+    try {
+      await user.update(userDB, parentDB, data, parent);
+    } catch (error) {
+      throw new DatabaseError("Error in updating data");
+    }
+  }
+  async getUserInfo(id) {
+    try {
+      // Get user model
+      const user = await UserModel.getUser(userDB, parentDB, id);
+      return user;
+    } catch (error) {
+      throw new DatabaseError("Error in getting data");
+    }
+  }
+};
