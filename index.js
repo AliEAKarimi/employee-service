40c2a6b1fbbe4ac5e6a9db73b403d1ad063c9fac
Ali Karimi <alieakarimi@gmail.com>
2023-04-09 11:24:16 +0330

    using implemented middlewares and services

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const Server = require("./server/server");
-const controller = require("./controllers/controller");
-const { RequestMethod } = require("./models/requestMethod");
+const UserController = require("./controllers/userController");
+const UserService = require("./services/userService");
+const { RequestMethod } = require("./helpers/requestMethod");
 const Router = require("./router/router");
 const { isOperationalError } = require("./errorHandlers/errorHandler");
+const { userSchema, getUserQuerySchema } = require("./schemas/schemas");
+const bodyParser = require("./middlewares/bodyParser");
+const dataValidator = require("./middlewares/dataValidator");
+const {
+  checkIdNotDuplicated,
+  checkParentExists,
+  checkIdExists,
+} = require("./middlewares/checkings");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -18,10 +27,42 @@ process.on("uncaughtException", (error) => {
 });
 
 const router = new Router();
+const userService = new UserService();
+const userController = new UserController(userService);
+
 // add routes
-router.addRoute("/dataService", RequestMethod.POST, controller.addUser);
-router.addRoute("/dataService", RequestMethod.GET, controller.getUser);
-router.addRoute("/dataService", RequestMethod.PUT, controller.updateUser);
+router.addRoute(
+  "/dataService",
+  RequestMethod.POST,
+  userController.addUser.bind(userController),
+  [
+    { function: bodyParser, config: { isParams: false } },
+    { function: dataValidator, config: { schema: userSchema } },
+    checkIdNotDuplicated,
+    checkParentExists,
+  ]
+);
+router.addRoute(
+  "/dataService",
+  RequestMethod.PUT,
+  userController.updateUser.bind(userController),
+  [
+    { function: bodyParser, config: { isParams: false } },
+    { function: dataValidator, config: { schema: userSchema } },
+    checkIdExists,
+    checkParentExists,
+  ]
+);
+router.addRoute(
+  "/dataService",
+  RequestMethod.GET,
+  userController.getUser.bind(userController),
+  [
+    { function: bodyParser, config: { isParams: true } },
+    { function: dataValidator, config: { schema: getUserQuerySchema } },
+    checkIdExists,
+  ]
+);
 
 const server = new Server(
   router.route.bind(router),
