7ea5ba44c6031d6cc0ef3db55fe8744d40eeb3f2
Ali Karimi <alieakarimi@gmail.com>
2023-04-09 11:13:41 +0330

    changed userSchema

--- schemas/schemas.js
+++ schemas/schemas.js
@@ -1,13 +1,19 @@
 const Joi = require("joi");
 
 const userSchema = Joi.object({
-  id: Joi.string().required(),
-  data: Joi.object().required(),
+  idNumber: Joi.string().required(),
+  data: Joi.object({
+    username: Joi.string().required(),
+    jobSkill: Joi.string().required(),
+    job: Joi.object({
+      company: Joi.string().required(),
+    }).required(),
+  }).required(),
   parent: Joi.string().required(),
 });
 
 const getUserQuerySchema = Joi.object({
-  id: Joi.string().required(),
+  idNumber: Joi.string().required(),
 });
 
 module.exports = {
