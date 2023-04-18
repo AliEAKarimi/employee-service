286b5da3394e0f10fb12e238c4cfd0d75e939830
Ali Karimi <alieakarimi@gmail.com>
2023-04-18 17:35:32 +0330

    remove username from data

--- schemas/schemas.js
+++ schemas/schemas.js
@@ -3,7 +3,6 @@ const Joi = require("joi");
 const userSchema = Joi.object({
   id: Joi.string().required(),
   data: Joi.object({
-    username: Joi.string().required(),
     idNumber: Joi.string().required(),
     jobSkill: Joi.string().required(),
     job: Joi.object({
