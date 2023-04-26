6c0a66e4721ed7a6637124ea606801325d01f86e
Ali Karimi <alieakarimi@gmail.com>
2023-04-26 13:48:01 +0330

    used of redis-om repository and schema for interact with redis databases

--- services/userService.js
+++ services/userService.js
@@ -1,31 +1,56 @@
 const { userDB, parentDB } = require("../database/databases");
-const UserModel = require("../models/userModel");
+const userRepository = require("../repositories/userRepository");
+const parentRepository = require("../repositories/parentRepository");
 const DuplicateError = require("../errorHandlers/duplicateError");
+
 module.exports = class UserService {
   async addUser({ id, data, parent }) {
-    // Create user model
-    const user = new UserModel(id, data, parent);
-    await user.save(userDB, parentDB);
+    const newUser = { ...data, job: JSON.stringify(data.job) };
+    const newParent = { parent };
+    await Promise.all([
+      userRepository.save(`${id}`, newUser),
+      parentRepository.save(`${id}`, newParent),
+    ]);
   }
 
   async updateUser({ id: oldUsername, data, parent, newUsername }) {
     if (
       newUsername &&
       newUsername !== oldUsername &&
-      (await UserModel.exists(userDB, newUsername))
+      (await userDB.exists(`user:${newUsername}`))
     ) {
       throw new DuplicateError(`the user id ${newUsername} is duplicated`);
     }
-    // Create user model
-    const user = await UserModel.getUser(userDB, parentDB, oldUsername);
-    await user.update(userDB, parentDB, data, parent, newUsername);
+    const userData = await userRepository.fetch(`${oldUsername}`);
+    userData.job = JSON.parse(userData.job);
+    Object.deepExtend(userData, data);
+    userData.job = JSON.stringify(userData.job);
+    Promise.all([
+      userRepository.save(`${newUsername ?? oldUsername}`, userData),
+      parentRepository.save(`${newUsername ?? oldUsername}`, {
+        parent: parent ?? userData.parent,
+      }),
+    ]);
+    if (newUsername && newUsername !== oldUsername) {
+      Promise.all([
+        userRepository.remove(`${oldUsername}`),
+        parentRepository.remove(`${oldUsername}`),
+      ]);
+    }
   }
   async getUserInfo(id) {
-    // Get user model
-    const user = await UserModel.getUser(userDB, parentDB, id);
+    const [userData, { parent: userParent }] = await Promise.all([
+      userRepository.fetch(`${id}`),
+      parentRepository.fetch(`${id}`),
+    ]);
+    userData.job = JSON.parse(userData.job);
+    const user = { id, userData, userParent };
     return user;
   }
   async deleteUser(id) {
-    await UserModel.delete(userDB, parentDB, id);
+    await Promise.all([
+      userRepository.remove(`${id}`),
+      parentRepository.remove(`${id}`),
+    ]);
   }
 };
