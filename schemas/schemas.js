36cd182c458f8aaad0eab9a25ae2a46dd2006b01
Ali Karimi <alieakarimi@gmail.com>
2023-04-27 17:27:21 +0430

    remove idNumber from userData

--- schemas/schemas.js
+++ schemas/schemas.js
@@ -4,7 +4,6 @@ const userSchema = Joi.object({
   id: Joi.string().required(),
   data: Joi.object({
     username: Joi.string().required(),
-    idNumber: Joi.string().required(),
     jobSkill: Joi.string().required(),
     job: Joi.object({
       company: Joi.string().required(),
